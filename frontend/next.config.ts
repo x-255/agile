449f950757b1de6787ab0568b53a948f35c7d03f
finn.xu <ouhuangff@163.com>
2026-01-29 10:44:19 +0800

    🐎 ci: build部署的必要配置修改

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   allowedDevOrigins: ['wecom.perficientus.com.cn'],
   basePath: process.env.BASE_PATH ?? '',
+  trailingSlash: true,
 }
 
 export default nextConfig
